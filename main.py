885bbf6e40fc2b07fac6363af80bcba69442f16b
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2024-10-14 21:21:15 +0200

    Implemented object grouping

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ import pygame
 from constants import *
 from player import Player
 
+
+
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gc = pygame.time.Clock()
     dt = 0
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     # main loop
     while True:
@@ -20,8 +27,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0)) 
-        player.update(dt)
-        player.draw(screen)
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = gc.tick(60) / 1000
 
