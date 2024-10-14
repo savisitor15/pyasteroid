7a606cce250f783ec9e34a30da731fb7a4b51877
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2024-10-14 19:10:49 +0200

    Ticker and frame limit

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gc = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0)) 
         pygame.display.flip()
+        dt = gc.tick(60) / 1000
 
 
 if __name__ == "__main__":
