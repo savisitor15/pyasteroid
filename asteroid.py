02b8c2d7f6afaaa2bc8cc9fee8053cb021f3237c
savisitor15 <46744807+savisitor15@users.noreply.github.com>
2024-10-15 10:30:12 +0200

    Implemented asteroid colors

--- asteroid.py
+++ asteroid.py
@@ -6,9 +6,10 @@ import pygame
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.color = random.choice(["white", "green","red","yellow"])
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, self.color, self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
